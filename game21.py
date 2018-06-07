c8d9d7ab48eac3c289594515dbbb35a541f3d2f3
Annant05 <annantguptaneema@gmail.com>
2018-06-07 22:37:26 +0530

    Refactor and cleanup

--- game21.py
+++ game21.py
@@ -2,31 +2,30 @@
 from random import choice
 
 
-def checkCardorReselect():
-    card = int(input("Enter a Card "))
-    if (card not in cards):
+def select_card():
+    acard = int(input("Enter a Card "))
+    if acard not in cards:
         print("Please Reselect Card")
-        return checkCardorReselect()
+        return select_card()
     else:
-        return card
+        return acard
 
 
 cards = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 playerScore = 0
 cpuScore = 0
-while (not (playerScore >= 21 or cpuScore >= 21)):
-    print("Player TURN Avaliable Cards :  " + str(cards))
-    card = checkCardorReselect()
+while not (playerScore >= 21 or cpuScore >= 21):
+    print("Player TURN Available Cards :  " + str(cards))
+    card = select_card()
     playerScore = playerScore + card
     cards.remove(card)
-    print("Player Score : " + str(playerScore))
 
     selectedCard = choice(cards)  # randomCard()
     cpuScore = cpuScore + selectedCard
     cards.remove(selectedCard)
-    print("CPU Score : " + str(cpuScore))
+    print("\nPlayer Score : " + str(playerScore) + "\tCPU Score : " + str(cpuScore))
 
-if (playerScore >= 21):
+if playerScore >= 21:
     print("\n Player Win \nScore : " + str(playerScore))
-elif (cpuScore >= 21):
+elif cpuScore >= 21:
     print("\n CPU Win \nScore : " + str(cpuScore))
