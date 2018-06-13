be0bf0c5c53146a5b8e277b0745395df7c15d935
Annant05 <annantguptaneema@gmail.com>
2018-06-13 11:23:56 +0530

    Words in dictionary and next words

--- word_after.py
+++ word_after.py
@@ -1,31 +1,40 @@
-def word_occur(word):
-    count = 0
-    indexes = []
-    while count != len(word_array):
-        if word.upper() == word_array[count]:
-            indexes.append(count)
-        count += 1
-    print(indexes)
-
-
 data = open('para.txt')
 lis_line = []
-for line in data:
-    lis_line.append(line.upper().replace(",", "").replace("\"", "").replace(".", "").split(' '))
-
 word_array = []
+for line in data:
+    lis_line.append(
+        line.upper().replace(",", "")
+            .replace("\"", "")
+            .replace(".", "")
+            .replace("(", "")
+            .replace(")", "")
+            .replace("\n", "").split(' '))
+
 for split_words in lis_line:
     for i in split_words:
         word_array.append(i)
 
-word_occur("an")
 
-# print("WORD COUNT " + str(len(split_words)))
-# word_array = split_words.copy()
-# word_after = []
-# count = 1
-# print(word_array[9])
-# while count != len(split_words):
-#     if word_array[]
-#
-#     count +=1
+# Consider the above code as declaration
+
+def next_word_list(word):
+    count = 0
+    indexes = []
+    next_words = []
+
+    while count != (len(word_array) - 1):
+        if word.upper() == word_array[count]:
+            indexes.append(count + 1)
+        count += 1
+
+    for i in indexes:
+        next_words.append(word_array[i])
+    return next_words
+
+
+word_dictionary = dict()
+for i in word_array:
+    word_dictionary[str(i)] = next_word_list(str(i))
+# print(word_dictionary)
+for i in word_dictionary:
+    print("Word is : " + i + "\tNext word :  " + str(word_dictionary[i]))
